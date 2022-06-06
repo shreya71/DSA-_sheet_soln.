a2bcd71329c45b09170b9d7c4c0d7df22228b585
Shreya <91543981+shreya71@users.noreply.github.com>
2022-06-06 21:40:20 +0530

    Update Leetcode 20. Valid Parentheses.cpp

--- Strings/Easy/Leetcode 20. Valid Parentheses.cpp
+++ Strings/Easy/Leetcode 20. Valid Parentheses.cpp
@@ -7,6 +7,29 @@ APPROACH: STACK concept used.
 
 ******************************************************************************************************************************/
 
+// Better Runtime and Memort Usage Rating
+//                                        Without iterator
+class Solution {
+public:
+    bool isValid(string s) {
+         stack<char> s1;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] =='(' || s[i] =='{' || s[i] =='[')
+                s1.push(s[i]);
+            else
+            {
+                if(s1.empty() || (s1.top() == '(' && s[i] != ')') || (s1.top() == '{' && s[i] != '}') || (s1.top() == '[' && s[i] != ']'))
+                    return false;
+            s1.pop();
+            }
+        }
+        return s1.empty();
+    }
+};
+
+// #####################################################################################################################################
+//                                                        Iterator used
 class Solution {
 public:
     bool isValid(string s) {
